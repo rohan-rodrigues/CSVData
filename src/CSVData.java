8b16071dc9d5e8f3b4ec211583d1c09eb36c94f3
Rohan Rodrigues <rohanrodrigues19@gmail.com>
2016-10-12 10:40:04 -0700

    Add method to CSVData

--- src/CSVData.java
+++ src/CSVData.java
@@ -4,5 +4,25 @@
  *
  */
 public class CSVData {
+	private String[] columnNames;
+	private double[][]data;
+	
+	public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] columnNames) {
+		return null;
+		
+	}
+	
+	
+	/***
+	 * Returns a new CSV data object for a file ignoring lines at the top.
+	 * It uses the first row as the column names. All other data is stored as doubles.
+	 * 
+	 * @param filename - the file to read
+	 * @param numLinesToIgnore - the number of lines at the top to ignore
+	 * @return a CSVData object for that file
+	 */
+	public static CSVData readCSVData(String filename, int numLinesToIgnore) {
+		return null;
+	}
 	
 }

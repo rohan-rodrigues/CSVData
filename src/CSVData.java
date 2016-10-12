74013e6079219337e9af3e499b2815d2a8e0b3af
Rohan Rodrigues <rohanrodrigues19@gmail.com>
2016-10-12 11:08:30 -0700

    Add Method stubs to CSVData

--- src/CSVData.java
+++ src/CSVData.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /***
  * A class to read/write numerical CSV files and allow easy access of data
  * @author rohanrodrigues
@@ -8,8 +10,8 @@ public class CSVData {
 	private double[][]data;
 	
 	public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] columnNames) {
-		return null;
-		
+		CSVData data = CSVData.readCSVFile(filename, numLinesToIgnore, columnNames);
+		return data;
 	}
 	
 	
@@ -22,7 +24,105 @@ public class CSVData {
 	 * @return a CSVData object for that file
 	 */
 	public static CSVData readCSVData(String filename, int numLinesToIgnore) {
+		CSVData data = CSVData.readCSVData(filename, numLinesToIgnore);
+		return data;
+	}
+	
+	public double[][] getData() {
+		return this.data;
+	}
+	
+	public String[] getColumnNames() {
+		return this.columnNames;
+	} 
+	
+	public static double[] getColumn(double[][] arr, int column) {
+		double[] specColumn = new double[arr.length];
+		for (int i = column; i < arr.length; i += arr.length)
+	}
+	
+
+	public double[] getRow(int rowIndex) {
 		return null;
 	}
+	
+	public double[] getColumn(int columnIndex) {
+		return null;
+		
+	}
+	public double[] getColumn(String name) {
+		return null;
+		
+	}
+	
+	public double[][] getRows(int startIndex, int endIndex) {
+		return null;
+		
+	}
+	
+	public double[][] getRows(int[] rowsIndexes) {
+		return null;
+	}
+	
+	public double[] getColumns(int startIndex, int endIndex) {
+		return null;
+	}
+	
+	public double[] getColumns(int colIndexes) {
+		return null;
+	}
+	
+	public double[] getColumns(String[] colNames) {
+		return null;
+	}
+	
+	public void setValue(int rowIndex, int colIndex, double newValue) {
+		
+	}
+	
+	public void setColName(int rowIndex, int colIndex, String name) {
+		
+	}
+	
+	
+	public void saveToFile(String fileName) {
+		
+	}
+	
+	
+	
+/*	public static double[] getRow(double[][] arr, int row) {
+		
+	}
+	
+	public static double[][] getRows(double[][] arr, int startRow, int endRow) {
+		
+	}
+	
+	public static double[][] getCols(double[][] arr, int startCol, int endCol) {
+		
+	}
+	
+	public static double[] getSpecificRow(int rowIndex) {
+		double[]row = data[rowIndex];
+		return row;
+	}
+	
+	public static double[][] getCols(double[][]arr, int startCol, int endCol) {
+		
+	}
+	
+	public static void setColumn(double[][]arr, int columnIndex, double[] vals) {
+		
+	}
+	
+	public static void setRow(double[][]arr, int rowIndex, double[] vals) {
+		
+	} */
+	
+/*	public double[] getSpecificColumns(int columnIndex) {
+		double[] column = data[columnIndex];
+		return column;
+	} */
 	
 }
